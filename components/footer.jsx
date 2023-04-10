77b5e8a38035a380eb8243dfc558a83de9e8eb5d
Hemant Pandey <hemantpandey1029@gmail.com>
2023-04-10 23:38:43 +0530

    modify footer page

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const Footer = () => {
     return (
-        <footer className="text-gray-600 body-font border-2 max-h-[10vh] print:hidden">
+        <div className="text-gray-600 body-font border-2 max-h-[10vh] print:hidden">
             <div className="container p-4 mx-auto flex items-center justify-between">
                 <h1>Made by <span className='font-bold text-xl text-red-800'>Hemant Pandey</span></h1>
                 <div className='flex items-center space-x-10'>
@@ -12,7 +12,7 @@ const Footer = () => {
                     <Link href={'https://github.com/Hemant2059/jntuh-r18-results'}><BsGithub className='scale-[1.5]'/></Link>
                 </div>
             </div>
-        </footer>
+        </div>
     )
 }
 
