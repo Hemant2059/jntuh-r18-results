c8110e951ddbae4f1886b3d2acb6f8b894f1443a
Hemant Pandey <hemantpandey1029@gmail.com>
2023-04-11 00:13:51 +0530

    user analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,13 @@
 import Navbar from "@/components/Navbar"
 import "../public/globals.css"
 import Footer from "@/components/Footer"
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Jntuh Results',
   description: "Looking for the latest JNTUH R18 results? Look no further than our website, where you can find the most accurate and up-to-date results for all JNTUH exams. Whether you're a student or a parent, our website makes it easy to track your academic progress and stay informed about your performance. So why wait? Visit us today and discover how we can help you succeed at JNTUH and beyond.",
   keywords: 'JNTUH, JNTUH results, JNTUH R18 result, B.Tech results, JNTUH B.Tech R18, Engineering results, JNTUH updates, JNTUH notifications, JNTUH exams, JNTUH academic progress, JNTUH student portal, JNTUH login, JNTUH news, JNTUH official website, JNTU Hyderabad'
- 
+
 }
 
 export default function RootLayout({ children }) {
@@ -15,6 +16,8 @@ export default function RootLayout({ children }) {
       <body>
         <Navbar />
         {children}
+
+        <Analytics />
         <Footer />
       </body>
     </html>
